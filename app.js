f34adbf38f040b547bbd27c3d72ceaadd435b895
poojaptdr <pooja.patidar2121@gmail.com>
2022-07-12 18:31:41 +0530

    changed

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ function calculateProfitAndLoss(initial, quantity, current){
         outputBox.innerHTML = `Hey the loss is ${loss} and the percent is ${lossPercentage}%`
 
     }else if(current > initial){
-            let profit = (current - initial)*quantity
+            let profit = (current - initial)*quantity;
             let profitPercentage = (profit/initial)*100
             // console.log(`Hey the profit is ${profit} and the percent is ${profitPercentage}%`)
             outputBox.innerHTML = `Hey the profit is ${profit} and the percent is ${profitPercentage}%`
         }
         else{
             // console.log('No pain no gain and no gain no pain')
-            outputBox.innerHTML = 'No pain no gain and no gain no pain'
+            outputBox.innerHTML = 'No pain no gain and no gain no pain 😊!!'
         }
     }
 
